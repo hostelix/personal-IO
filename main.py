2f349d4f1925fc3732f0fff16c3e60b647fd25a8
Israel Lugo <hostelixisrael@gmail.com>
2016-04-10 22:13:06 -0430

    [ADD] agregado el archivo main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+import sys
+from interfaces.dialogo_registro_datos import *
+from interfaces.pantalla_principal import *
+
+
+class VentanaPrincipal(QtGui.QMainWindow):
+	def __init__(self, parent = None):
+		QtGui.QWidget.__init__(self,parent)
+		self.ui = Ui_VentanaPrincipal()
+		self.ui.setupUi(self)
+
+		QtCore.QObject.connect(self.ui.action_salir, QtCore.SIGNAL("triggered()"),self.salir_app)
+		QtCore.QObject.connect(self.ui.action_registrar_personal, QtCore.SIGNAL("triggered()"),self.abrir_dialogo_registro_personal)
+
+	def salir_app(self):
+		exit()
+
+	def abrir_dialogo_registro_personal(self):
+		dRegistro_personal = DialogoRegistroDatos().exec_()
+
+class DialogoRegistroDatos(QtGui.QDialog):
+	def __init__(self, parent = None):
+		QtGui.QWidget.__init__(self,parent)
+		self.ui = Ui_DialogoRegistroDatos()
+		self.ui.setupUi(self)
+
+		#QtCore.QObject.connect(self.ui.action_salir, QtCore.SIGNAL("triggered()"),self.cerrar_dialogo)
+
+	def cerrar_dialogo(self):
+		exit()
+
+if __name__ == '__main__':
+	app = QtGui.QApplication(sys.argv)
+	main = VentanaPrincipal()
+
+	main.show()
+
+	sys.exit(app.exec_())
+
+
