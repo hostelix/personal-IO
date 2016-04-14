0f9d5c2c09fdf63fb0f628600fc4d917e387a445
Israel Lugo <hostelixisrael@gmail.com>
2016-04-13 20:32:39 -0430

    [MOD] agregado el .idea al gitignore, tambien se corrigio la indexacion del main.py y se incluyeron algunas lib

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 #! /usr/bin/python
 import sys
+from PyQt4 import QtCore, QtGui
 from interfaces.dialogo_registro_datos import *
 from interfaces.pantalla_principal import *
 
 
 class VentanaPrincipal(QtGui.QMainWindow):
-	def __init__(self, parent = None):
-		QtGui.QWidget.__init__(self,parent)
-		self.ui = Ui_VentanaPrincipal()
-		self.ui.setupUi(self)
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_VentanaPrincipal()
+        self.ui.setupUi(self)
 
-		QtCore.QObject.connect(self.ui.action_salir, QtCore.SIGNAL("triggered()"),self.salir_app)
-		QtCore.QObject.connect(self.ui.action_registrar_personal, QtCore.SIGNAL("triggered()"),self.abrir_dialogo_registro_personal)
+        QtCore.QObject.connect(self.ui.action_salir, QtCore.SIGNAL("triggered()"), self.salir_app)
+        QtCore.QObject.connect(self.ui.action_registrar_personal, QtCore.SIGNAL("triggered()"),
+                               self.abrir_dialogo_registro_personal)
 
-	def salir_app(self):
-		msgBox = QtGui.QMessageBox()
-		msgBox.setWindowTitle("Mensaje de alerta")
-		msgBox.setText("Se va a cerrar la aplicacion")
-		msgBox.exec_()
-		exit()
+    def salir_app(self):
+        msgBox = QtGui.QMessageBox()
+        msgBox.setWindowTitle("Mensaje de alerta")
+        msgBox.setText("Se va a cerrar la aplicacion")
+        msgBox.exec_()
+        exit()
 
-	def abrir_dialogo_registro_personal(self):
-		dRegistro_personal = DialogoRegistroDatos().exec_()
+    def abrir_dialogo_registro_personal(self):
+        Registro_personal = DialogoRegistroDatos().exec_()
+
 
 class DialogoRegistroDatos(QtGui.QDialog):
-	def __init__(self, parent = None):
-		QtGui.QWidget.__init__(self,parent)
-		self.ui = Ui_DialogoRegistroDatos()
-		self.ui.setupUi(self)
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_DialogoRegistroDatos()
+        self.ui.setupUi(self)
 
-		#QtCore.QObject.connect(self.ui.action_salir, QtCore.SIGNAL("triggered()"),self.cerrar_dialogo)
+        # QtCore.QObject.connect(self.ui.action_salir, QtCore.SIGNAL("triggered()"),self.cerrar_dialogo)
 
-	def cerrar_dialogo(self):
-		exit()
+    def cerrar_dialogo(self):
+        exit()
+
 
 if __name__ == '__main__':
-	app = QtGui.QApplication(sys.argv)
-	main = VentanaPrincipal()
+    app = QtGui.QApplication(sys.argv)
+    main = VentanaPrincipal()
 
-	main.show()
+    main.show()
 
-	sys.exit(app.exec_())
-
-
+    sys.exit(app.exec_())
