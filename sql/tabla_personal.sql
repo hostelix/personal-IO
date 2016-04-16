cee81f99d2bbdcab4dfc5f401e5fb680083a6bdb
Israel Lugo <hostelixisrael@gmail.com>
2016-04-15 21:09:53 -0430

    [MOD] se agrego el sql de la tabla personal

--- sql/tabla_personal.sql
+++ sql/tabla_personal.sql
@@ -1,1 +1,13 @@
 
+CREATE TABLE personal (
+   id INT PRIMARY KEY NOT NULL,
+   primer_nombre CHAR(15),
+   segundo_nombre CHAR(15),
+   primer_apellido CHAR(15),
+   segundo_apellido CHAR(15),
+   cedula CHAR(8),
+   id_cargo INT,
+   id_nivel_instruccion INT,
+   email CHAR(20),
+   id_sexo INT
+);
