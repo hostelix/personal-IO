c9ea837e37df9733571f8a58ae576f364bccc96f
Israel Lugo <hostelixisrael@gmail.com>
2016-04-16 02:46:27 -0430

    [MOD] agregados nuevos path para los diferentes iconos

--- libs/paths.py
+++ libs/paths.py
@@ -15,3 +15,8 @@ PATH_CARPETA_APP = path.join(CARPETA_PERSONAL_USER, CARPETA_APP)
 PATH_DB = path.join(PATH_CARPETA_APP, NOMBRE_DB_APP)
 
 PATH_ARCHIVO_CONFIG = path.join(PATH_CARPETA_APP, NOMBRE_ARCHIVO_CONFIG)
+
+PATH_ICON_APP_16X16 = path.join('iconos', 'personal_io_16x16.png')
+PATH_ICON_APP_32X32 = path.join('iconos', 'personal_io_32x32.png')
+PATH_ICON_APP_64X64 = path.join('iconos', 'personal_io_64x64.png')
+PATH_ICON_APP_128X128 = path.join('iconos', 'personal_io_128x128.png')
