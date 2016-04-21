66b5f300842c1d42d14d408ff9a89fe0d5847354
Israel Lugo <hostelixisrael@gmail.com>
2016-04-21 17:31:44 -0430

    [ADD] se agrego la tabla de administrador

---
+++ sql/tabla_administradores.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS administradores (
+   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   usuario CHAR(20),
+   password CHAR(50),
+   email CHAR(30)
+);
+
+INSERT INTO administradores (usuario, password, email) VALUES ('admin', 'admin', 'admin@gmail.com');
