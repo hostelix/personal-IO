7841f89fd5471cf9920b42ba2a6fff519e529323
Israel Lugo <hostelixisrael@gmail.com>
2016-04-21 17:41:29 -0430

    [MOD] Se agrego la funcion de validar_id_admin y se movio la clase dialogoVerificarUsuario

--- libs/lib_extras.py
+++ libs/lib_extras.py
@@ -1,5 +1,6 @@
-from PyQt4 import QtGui, QtCore
 from libs import paths
+from interfaces.dialogo_verificar_usuario import *
+
 
 def confirmar_salida_app(_self):
     respuesta = QtGui.QMessageBox.question(_self, 'Confirmar la salida',
@@ -24,3 +25,40 @@ def traducir_aplicacion(_aplicacion):
     path = QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.TranslationsPath)
     translator.load('qt_%s' % locale, path)
     _aplicacion.installTranslator(translator)
+
+
+def verificar_is_admin(base_datos):
+    dialogo = DialogoVerificarUsuario(base_datos)
+    dialogo.exec_()
+    return dialogo.verificar_credenciales()
+
+
+class DialogoVerificarUsuario(QtGui.QDialog):
+    def __init__(self, base_de_datos, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_DialogoVerificarAdmin()
+        self.ui.setupUi(self)
+        # Seteamos el icono de la aplicacion
+        setear_icono_app(self)
+
+        self.personal_io_db = base_de_datos
+
+        QtCore.QObject.connect(self.ui.btn_verificar, QtCore.SIGNAL("clicked()"),
+                               self.verificar_credenciales)
+
+    def verificar_credenciales(self):
+        datos = {
+            'usuario': str(self.ui.input_usuario.text()),
+            'password': str(self.ui.input_password.text()),
+        }
+
+        existe_admin = self.personal_io_db.autenticar_administrador(datos)
+
+        # if 'hostelix' == datos['usuario'] and 'canaima' == datos['password']:
+        if existe_admin:
+            print "Acceso concedido"
+            self.close()
+            return True
+        else:
+            print  "Acceso denegado"
+            return False
