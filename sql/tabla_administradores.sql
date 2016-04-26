cddb8fbfa2a3085c87609324839e7e9380826241
Israel Lugo <hostelixisrael@gmail.com>
2016-04-25 22:01:01 -0430

    [MOD] Se encripto la contraseña del primer administrar

--- sql/tabla_administradores.sql
+++ sql/tabla_administradores.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS administradores (
    email CHAR(30)
 );
 
-INSERT INTO administradores (id,usuario, password, email) VALUES (1,'admin', 'admin', 'admin@gmail.com');
+INSERT INTO administradores (id,usuario, password, email) VALUES (1,'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com');
