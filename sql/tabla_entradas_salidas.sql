6c2de5f5cadd83c942e8b3b879bddfffd70ba094
Israel Lugo <hostelixisrael@gmail.com>
2016-04-29 20:56:49 -0430

    [MOD] se agrego un nuevo campo a la tabla de entrdas salidas, correspondiente a fecha

--- sql/tabla_entradas_salidas.sql
+++ sql/tabla_entradas_salidas.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS entradas_salidas (
    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    cedula CHAR(8),
    hora_entrada TEXT,
-   hora_salida TEXT
+   hora_salida TEXT,
+   fecha TEXT
 );
