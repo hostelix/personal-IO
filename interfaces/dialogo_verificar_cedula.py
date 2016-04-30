9284af0e94a6555990bfd70f330be6fae6e26832
Israel Lugo <hostelixisrael@gmail.com>
2016-04-29 22:10:13 -0430

    [FIX] se corrgio el nombre del dialogo que estaba mal escrito

--- interfaces/dialogo_verificar_cedula.py
+++ interfaces/dialogo_verificar_cedula.py
@@ -16,8 +16,6 @@ except AttributeError:
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
-
-
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
@@ -25,29 +23,29 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName(_fromUtf8("Dialog"))
-        Dialog.resize(322, 238)
-        Dialog.setStyleSheet(_fromUtf8("#input_cedula{background-color: white;}\n"
-                                       "#label_cedula{font-size: 15px;}"))
-        self.btn_aceptar = QtGui.QPushButton(Dialog)
+class Ui_DialogoVerificarCedula(object):
+    def setupUi(self, DialogoVerificarCedula):
+        DialogoVerificarCedula.setObjectName(_fromUtf8("DialogoVerificarCedula"))
+        DialogoVerificarCedula.resize(322, 238)
+        DialogoVerificarCedula.setStyleSheet(_fromUtf8("#input_cedula{background-color: white;}\n"
+                                                       "#label_cedula{font-size: 15px;}"))
+        self.btn_aceptar = QtGui.QPushButton(DialogoVerificarCedula)
         self.btn_aceptar.setGeometry(QtCore.QRect(90, 150, 141, 41))
         self.btn_aceptar.setObjectName(_fromUtf8("btn_aceptar"))
-        self.input_cedula = QtGui.QLineEdit(Dialog)
+        self.input_cedula = QtGui.QLineEdit(DialogoVerificarCedula)
         self.input_cedula.setGeometry(QtCore.QRect(60, 80, 201, 31))
         self.input_cedula.setInputMethodHints(QtCore.Qt.ImhNone)
         self.input_cedula.setAlignment(QtCore.Qt.AlignCenter)
         self.input_cedula.setPlaceholderText(_fromUtf8(""))
         self.input_cedula.setObjectName(_fromUtf8("input_cedula"))
-        self.label_cedula = QtGui.QLabel(Dialog)
+        self.label_cedula = QtGui.QLabel(DialogoVerificarCedula)
         self.label_cedula.setGeometry(QtCore.QRect(50, 40, 361, 21))
         self.label_cedula.setObjectName(_fromUtf8("label_cedula"))
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(DialogoVerificarCedula)
+        QtCore.QMetaObject.connectSlotsByName(DialogoVerificarCedula)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "Verificacion Cedula", None))
-        self.btn_aceptar.setText(_translate("Dialog", "Aceptar", None))
-        self.label_cedula.setText(_translate("Dialog", "Introduzca su cedula de Identidad", None))
+    def retranslateUi(self, DialogoVerificarCedula):
+        DialogoVerificarCedula.setWindowTitle(_translate("DialogoVerificarCedula", "Verificacion Cedula", None))
+        self.btn_aceptar.setText(_translate("DialogoVerificarCedula", "Aceptar", None))
+        self.label_cedula.setText(_translate("DialogoVerificarCedula", "Introduzca su cedula de Identidad", None))
