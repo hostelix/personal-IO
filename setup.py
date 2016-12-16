6524c301ffffc822ce6e6bd8a559085f56fca47c
Victor Pino <victopin0@gmail.com>
2016-12-16 15:11:03 -0400

    feat(debian) Debianized the project
    Add all struct to build a package.

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 personal-IO
+#
 
-# import py2exe
+import os
+from setuptools import setup
 
-setup(name="personal_io",
-      version="1.0",
-      description="Sistema para la administracion de entrada y salidas de personal",
-      author="Israel Lugo",
-      author_email="hostelixisrael@gmail.com",
-      url="https://github.com/hostelix/personal-IO/",
-      license="MIT",
-      scripts=["main.py"],
-      console=["main.py"],
-      options={"py2exe": {"bundle_files": 1}},
-      zipfile=None,
-      )
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="personal-IO",
+    packages=['personal-IO'],
+    version="0.1",
+    zip_safe=False,
+    long_description=README,
+    include_package_data=True,
+    license='MIT',
+    description="Sistema para la administracion de entrada y salidas de personal",
+    url='https://github.com/hostelix/personal-IO/',
+    author="Israel Lugo",
+    author_email="hostelixisrael@gmail.com",
+)
